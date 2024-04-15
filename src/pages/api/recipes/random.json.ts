be6b5a86d6167fadf2af1a99112e370ee6fa0100
Nicolas Quintero <quinterorodrigueznicolas@gmail.com>
2024-04-15 15:51:16 -0500

    remove test

--- src/pages/api/recipes/random.json.ts
+++ src/pages/api/recipes/random.json.ts
@@ -5,8 +5,6 @@ export async function GET({ params, request }: APIContext) {
   const response = await fetch(`${config.apiBase}/random.php`)
   const data = await response.json()
 
-  console.log(config.apiBase)
-
   return new Response(JSON.stringify(data.meals[0]), {
     status: 200,
     headers: {
